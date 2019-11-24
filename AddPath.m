73a4dd656f7acfdf353a8757409d97c720160dfc
lyhehehe <yanglu.hust@gmail.com>
2019-11-24 10:43:40 +0800

    Add the \FRM

--- AddPath.m
+++ AddPath.m
@@ -1,1 +1,1 @@
-addpath('.\coil','.\OriginData','.\WorkData','.\Force','.\FreqResFun','.\Plot','.\Vibration','.\Function','.\color');
+addpath('.\coil','.\OriginData','.\WorkData','.\Force','.\FreqResFun','.\Plot','.\Vibration','.\Function','.\color','.\FRM');
