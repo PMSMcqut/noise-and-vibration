4ce55d2dbd8fe09e72e918464a8eea74099d963e
lyhehehe <yanglu.hust@gmail.com>
2020-01-06 11:05:53 +0800

    Define variables

--- Main_MultiSpeed.m
+++ Main_MultiSpeed.m
@@ -14,6 +14,10 @@ FRF_method='equation' % equation, Wave Excitation, Tooth Excitation
 % =================================================================
 speed_min=300;speed_max=1500;speed_step=100;
 speed=speed_min:speed_step:speed_max;
+
+Force_wave=cell(1,length(speed));
+Force_map=cell(1,length(speed));
+Vib_map=cell(1,length(speed));
 for i=1:length(speed)
     f0=p*speed(i)/60;% fundamental electrical frequncy
     T=1/f0;% electrical period
@@ -33,17 +37,18 @@ for i=1:length(speed)
                     Br=Bg.rad';
                     Bt=Bg.tan';
                 case 'sin'
-                    [Iph,Islot]=Current(n3ph,m,p,Nt,Ipeak,f0,T,time(1:end),gamma,'sin',I_harm{i},SlotMatrix,ap);
+                    [Iph,Islot]=Current(n3ph,m,p,Nt,Ipeak,f0,T,time(1:end),gamma,'harm',I_harm{i},SlotMatrix,ap,Nlayer);
                     [Barm,Bpm,Bg,Barm_slot]=FluxCal(time(1:end),Barm_fun,Bpm_fun,Ipeak,Islot,Basic_speed,speed(i),Nt);
                     Br=Bg.rad';
                     Bt=Bg.tan';
             end
             Test=TestLoad;
         case 'noload'
-            [Iph,Islot]=Current(n3ph,m,p,Nt,Ipeak,f0,T,time(1:end),gamma,'harm',I_harm{i},SlotMatrix,ap);
+            [Iph,Islot]=Current(n3ph,m,p,Nt,Ipeak,f0,T,time(1:end),gamma,'harm',I_harm{i},SlotMatrix,ap,Nlayer);
             [Barm,Bpm,Bg,Barm_slot]=FluxCal(time(1:end),Barm_fun,Bpm_fun,Ipeak,Islot,Basic_speed,speed(i),Nt);
-            Br=Bom.rad';
-            Bt=Bpm.tan';
+            %             [Bpm,Bg]=FluxCal_noload(time(1:end),Bpm_fun,Ipeak,Basic_speed,speed(i),Nt);
+            Br=Bg.rad';
+            Bt=Bg.tan';
             Test=TestNoload;
     end
     % ====================== Calculation parameter =====================
@@ -56,8 +61,6 @@ for i=1:length(speed)
     TimeDomain.Space=Space(1:AngleStep);% Space angle for FFT
     TimeDomain.ToothAngle=ToothAngle(1:Qs);% Tooth angle for FFT
     %% Force Calcualtion
-    Force_wave=cell(1,length(speed));
-    Force_map=cell(1,lengtn(speed));
     [F,Frz,Ftz,Mrz,ForceIntegral,FourierBr,FourierBt,FourierFr,FourierFt,FourierRad,FourierTan,FourierTq,Force]...
         =Force_Calculation_MultiSpeed(Br,Bt,Freq,ForceNum,mu0,L,Ris,Qs,alpha_b0,Space,TimeStep,AngleStep,TimeDomain,f0);
     Force_wave{i}.rad=Frz;
@@ -66,14 +69,13 @@ for i=1:length(speed)
     Force_map{i}=Force;
     %% Frequency Response Function
     FRF_freq=Freq;
-%     damp=[6.6,2.7,7.1,7.3]*1e-02;% damping ratio
-%     damp=zeros(1,length(FRF_freq));
-%     damp(1:79)=(2.76*10^-5*FRF_freq(1:79)+0.062)*(1/(2*pi));
-%     damp(80:end)=log(0.0001*FRF_freq(80:end)+1)+0.062;
+    %     damp=[6.6,2.7,7.1,7.3]*1e-02;% damping ratio
+    %     damp=zeros(1,length(FRF_freq));
+    %     damp(1:79)=(2.76*10^-5*FRF_freq(1:79)+0.062)*(1/(2*pi));
+    %     damp(80:end)=log(0.0001*FRF_freq(80:end)+1)+0.062;
     damp=log(0.0001*FRF_freq+1)+0.062;
     [FreqResEq,FreqResWave,FreqResTooth]=FreqResFun_Calculation(Mode,FRF_freq,FreqResWave,FreqResTooth,StaDef,f_modal,damp);
     %% Vibration Calculation
-    Vib_map=cell(1,length(speed));
     switch FRF_method
         case 'equation'
             FRF.rad=FreqResEq.rad.complex(1:length(Freq),:)';
